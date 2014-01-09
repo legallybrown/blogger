517754a4659dd9e50f316be7d085aa04a33e19ec
Anoop Joshi <anoopjoshi@hotmail.co.uk>
2014-01-09 16:35:20 +0000

    Added ability to edit and update existing article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,17 +15,27 @@ class ArticlesController < ApplicationController
   end
 
   def create
-  @article = Article.new(article_params)
-  @article.save
-  redirect_to article_path(@article)
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
   end
+
+  def edit
+    @article = Article.find(params[:id]) 
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+
+  end   
+
 
   def destroy
     @articles = Article.destroy(params[:id])
     redirect_to articles_path(@article)
-  end
-
-  def confirm_deletion
   end 
 
 end
